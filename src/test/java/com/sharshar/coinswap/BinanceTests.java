6b70b708ced7dbbc1f9e1c1a516b87c23e44bf86
ncmec-lsharshar <lsharshar@ncmec.org>
2018-07-22 14:51:30 -0400

    Initial Commit

---
+++ src/test/java/com/sharshar/coinswap/BinanceTests.java
@@ -0,0 +1,34 @@
+package com.sharshar.coinswap;
+
+import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.domain.account.Account;
+import com.binance.api.client.domain.account.Order;
+import com.binance.api.client.domain.account.Trade;
+import com.sharshar.coinswap.beans.PriceData;
+import com.sharshar.coinswap.exchanges.binance.BinanceAccountServices;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+/**
+ * Created by lsharshar on 7/19/2018.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BinanceTests {
+	@Autowired
+	BinanceAccountServices binanceAccountServices;
+
+	@Autowired
+	private BinanceApiRestClient binanceApiRestClient;
+
+	@Test
+	public void testOrderDetails() {
+		List<Trade> trades = binanceAccountServices.getMyTrades("BNBBTC", "58100614");
+		System.out.println("Trades: " + trades);
+	}
+}
