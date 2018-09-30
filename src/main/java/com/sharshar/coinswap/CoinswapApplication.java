5460ade099eaee970387b867e2edc064059b2912
ncmec-lsharshar <lsharshar@ncmec.org>
2018-09-30 19:45:22 -0400

    Set up asynchronous processing

--- src/main/java/com/sharshar/coinswap/CoinswapApplication.java
+++ src/main/java/com/sharshar/coinswap/CoinswapApplication.java
@@ -2,10 +2,12 @@ package com.sharshar.coinswap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class CoinswapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoinswapApplication.class, args);
